65bf8211a631789a666f18a8f28d3580cb7465b9
Joakim Paulsson <joakim.jp.paulsson@gmail.com>
2024-03-28 20:18:02 +0100

    will stop here for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,65 +1,20 @@
--- pick your plugin manager
-local pack = "lazy"
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-local function bootstrap(url, ref)
-	local name = url:gsub(".*/", "")
-	local path
-
-	if pack == "lazy" then
-		path = vim.fn.stdpath("data") .. "/lazy/" .. name
-		vim.opt.rtp:prepend(path)
+local function bootstrap(author, plugin, ...)
+	local path = (vim.fn.stdpath("data") .. "/lazy/" .. plugin)
+	if not vim.loop.fs_stat(path) then
+		local url = ("https://github.com/" .. author .. "/" .. plugin .. ".git")
+		print("cloning", url, "to", path)
+		vim.fn.system({ "git", "clone", url, path, "--filter=blob:none", ... })
 	else
-		path = vim.fn.stdpath("data") .. "/site/pack/" .. pack .. "/start/" .. name
 	end
-
-	if vim.fn.isdirectory(path) == 0 then
-		print(name .. ": installing in data dir...")
-
-		vim.fn.system { "git", "clone", url, path }
-		if ref then
-			vim.fn.system { "git", "-C", path, "checkout", ref }
-		end
-
-		vim.cmd "redraw"
-		print(name .. ": finished installing")
-	end
+	return (vim.opt.rtp):prepend(path)
 end
-
--- for stable version [recommended]
-bootstrap("https://github.com/udayvir-singh/tangerine.nvim")
-
--- for git head
-bootstrap("https://github.com/udayvir-singh/tangerine.nvim")
-
-require("tangerine").setup(
-	{
-		-- save fnl output in a separate dir, it gets automatically added to package.path
-		target = vim.fn.stdpath [[data]] .. "/tangerine",
-
-		-- compile files in &rtp
-		rtpdirs = {
-			"ftplugin",
-		},
-
-		compiler = {
-			-- disable popup showing compiled files
-			verbose = false,
-
-			-- compile every time you change fennel files or on entering vim
-			hooks = { "onsave", "oninit" }
-		}
-	}
-)
+bootstrap("folke", "lazy.nvim", "--branch=stable")
+bootstrap("rktjmp", "hotpot.nvim")
+local hotpot = require("hotpot")
+hotpot.setup({
+	provide_require_fennel = true,
+	compiler = { modules = { correlate = true } },
+	enable_hotpot_diagnostics = true
+})
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+require("main")
